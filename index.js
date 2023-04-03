4d341f606c83aa873a458599a8c4bc6705610ebf
Quinterlozi <jojobobo254@gmail.com>
2023-04-03 14:12:29 +0300

    Function that seeks to find names that mean the same when spelled from front to back and vice versa

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //This challenge checks to see names that sound the same when spelled from front to back and back to front
 }
 
 /* 
   Add your pseudocode here
-*/
+//Write a function know as isPalindrome
+//Convert the string to lowerCase
+//Loop through the dtring to check if the first and last letter are a match
+//If the loop ends and the letters don't match return false
+//Else if the loop ends and the letters match return true
 
 /*
   Add written explanation of your solution here
-*/
+*/function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+  
+  // Loop through the string and check if the first and last characters match
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  
+  // If we make it through the loop without returning false, the string is a palindrome
+  return true;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
